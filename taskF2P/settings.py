2af0b29cc7b5e304f1881f806198b18f259fcc56
Ahmed Khateeb <khateeb321@gmail.com>
2018-09-23 16:29:06 +0500

    password reset

--- taskF2P/settings.py
+++ taskF2P/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/f2p'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
